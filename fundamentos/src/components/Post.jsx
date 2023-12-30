349bc861b2e9fe3ef1ea8fb33051683506c5f299
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2023-12-29 21:05:40 -0300

    feat: create Comment component

--- fundamentos/src/components/Post.jsx
+++ fundamentos/src/components/Post.jsx
@@ -5,31 +5,38 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="http://github.com/JamissonBarbosa.png"  />
+          <img
+            className={styles.avatar}
+            src="http://github.com/JamissonBarbosa.png"
+          />
           <div className={styles.authorInfo}>
             <strong>Jamisson Barbosa</strong>
             <span>Web Develper</span>
           </div>
         </div>
         <div>
-        <time title="14 de Novembro 13:27" dateTime="2025-11-14 13:27:00">Publicado á 1h
-        </time>
+          <time title="14 de Novembro 13:27" dateTime="2025-11-14 13:27:00">
+            Publicado á 1h
+          </time>
         </div>
       </header>
       <div className={styles.content}>
         <p> Fala galera</p>
-        <p>Acabei de subir mais um projeto no meu portifólio 
+        <p>Acabei de subir mais um projeto no meu portifólio</p>
+        <p>
+          <a href="#">Jamisson.Desenvolvedor</a>
         </p>
-        <p><a href="#">Jamisson.Desenvolvedor</a></p>
-        <p><a href="#">#novoProjeto #NLW</a></p>
+        <p>
+          <a href="#">#novoProjeto #NLW</a>
+        </p>
       </div>
 
-      <form className={styles.comentForm}>
+      <form className={styles.commentForm}>
         <strong>Deixe seu FeedBack</strong>
-        <textarea 
-        placeholder="Deixe um comentário"
-        />
-        <button type="submit">Comentar</button>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
       </form>
     </article>
   );
